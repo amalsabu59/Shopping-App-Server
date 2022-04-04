c1cfb3c2a0de7267afdd51711a30afaa9a8bdc76
amalsabu59 <amalsabu59@gmail.com>
2022-04-04 21:02:20 +0530

    razorpay payment integrated

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const productsRoute = require("./routes/products")
 const cartRoute = require("./routes/cart")
 const orderRoute = require("./routes/orders")
 const stripeRoute = require("./routes/stripe")
+const paymentRoutes = require("./routes/razorpay")
 
 dotenv.config()
 
@@ -25,6 +26,8 @@ app.use("/api/products",productsRoute)
 app.use("/api/carts",cartRoute)
 app.use("/api/orders",orderRoute)
 app.use("/api/checkout",stripeRoute)
+app.use("/api/payment",paymentRoutes);
+
 
 app.listen(process.env.PORT || 8800, ()=> {
     console.log("backend server is running !") 
