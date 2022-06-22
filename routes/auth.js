592cdadbd8d7556e657539850635d6c2cef8e885
amalsabu59 <amalsabu59@gmail.com>
2022-06-22 22:07:08 +0530

    verify token updated

--- routes/auth.js
+++ routes/auth.js
@@ -21,36 +21,37 @@ router.post("/register",async (req,res)=>{
 
 //LOGIN
 
-router.post("/login",async(req,res)=>{
-    try{
-        const user = await User.findOne({ username:req.body.username })
-
-        !user && res.status(401).json("Wrong Credentials")
-
-        const hashedPasword = CryptoJS.AES.decrypt(user.password,
-            process.env.PASS_SEC)
-
-        const OrginalPassword = hashedPasword.toString(CryptoJS.enc.Utf8)    
-
-        OrginalPassword !== req.body.password && 
-        res.status(401).json("Wrong Credentials")
-
-        const accessToken = jwt.sign(
-            {
-            id: user._id,
-             isAdmin: user.isAdmin
+router.post("/login", async (req, res) => {
+    try {
+      const user = await User.findOne({ username: req.body.username });
+      !user && res.status(401).json("Wrong credentials!");
+  
+      const hashedPassword = CryptoJS.AES.decrypt(
+        user.password,
+        process.env.PASS_SEC
+      );
+      const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+  
+      OriginalPassword !== req.body.password &&
+        res.status(401).json("Wrong credentials!");
+  
+      const accessToken = jwt.sign(
+        {
+          id: user._id,
+          isAdmin: user.isAdmin,
         },
         process.env.JWT_SEC,
         {expiresIn:"3d"}
-        );
-
-        const {password, ...others} = user._doc
-
-        res.status(200).json({...others, accessToken})
-        
-    }catch(err) {
-        res.status(500).json(err)
+      );
+  
+      const { password, ...others } = user._doc;
+  
+      res.status(200).json({...others, accessToken});
+    } catch (err) {
+      res.status(500).json(err);
     }
-})
+  });
+  
+
 
 module.exports = router 
