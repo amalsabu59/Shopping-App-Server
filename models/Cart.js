2bf67c20f2df82a4affd83a5444e988feccaa14d
amalsabu59 <amalsabu59@gmail.com>
2023-03-22 22:36:22 +0530

    cart schema updated

--- models/Cart.js
+++ models/Cart.js
@@ -1,27 +1,25 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const CartSchema = new mongoose.Schema({
-    userId:{
-        type: String,
-        required: true,
-       
+const CartSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    products:[
-        {
-           ProductId :{
-               type:String,
-           },
-           quantity:{
-               type: Number,
-               default:1,
-           }
+    products: [
+      {
+        ProductId: {
+          type: String,
+        },
+        quantity: {
+          type: Number,
+          default: 1,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
-        }
-    ]
-    
-    
-    
-    
-},{timestamps: true})
-
-module.exports = mongoose.model("Cart",CartSchema)
+module.exports = mongoose.model("Cart", CartSchema);
